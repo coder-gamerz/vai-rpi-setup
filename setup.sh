42411759670e0a514967c07efda1f321aa3fec06
coder-gamerz <98739886+coder-gamerz@users.noreply.github.com>
2023-08-06 14:47:06 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt update && sudo apt upgrade
+sudo apt install libespeak1
+sudo apt-get install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+sudo apt-get install ffmpeg libav-tools
